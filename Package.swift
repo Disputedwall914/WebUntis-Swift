f92c4d1464b3c42285663670829afbf1a4321f85
Disputedwall914 <93341247+Disputedwall914@users.noreply.github.com>
2025-04-07 18:45:55 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,33 +1,15 @@
 // swift-tools-version:5.7
+
 import PackageDescription
 
 let package = Package(
-    name: "WebUntis",
-    platforms: [
-        .iOS(.v14), .macOS(.v11)
-    ],
-    products: [
-        .library(
-            name: "WebUntis",
-            targets: ["WebUntis"]
-        )
-    ],
-    dependencies: [
-        .package(url: "https://github.com/google/promises.git", from: "1.2.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.2.0"),
-        .package(url: "https://github.com/realm/realm-cocoa.git", from: "5.4.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.0")
-    ],
-    targets: [
-        .target(
-            name: "WebUntis",
-            dependencies: [
-                .product(name: "Promises", package: "promises"),
-                "Alamofire",
-                .product(name: "RealmSwift", package: "realm-cocoa"),
-                "CryptoSwift"
-            ],
-            path: "Sources/WebUntis"
-        )
-    ]
+	name: "WebUntis",
+    majorVersion: 1,
+    minor: 1,
+	dependencies: [
+		.Package(url: "https://github.com/google/promises.git", majorVersion: 1, minor: 2),
+		.Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 5, minor: 2),
+		.Package(url: "https://github.com/realm/realm-cocoa.git", majorVersion: 5, minor: 4),
+		.Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 1, minor: 3),
+	]
 )
